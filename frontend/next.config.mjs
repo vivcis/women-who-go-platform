ece251f7d097f741250ae36dfd773b2dfaad68c3
vivcis <ceciliaorji.co@gmail.com>
2025-11-09 22:25:07 +0100

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
   trailingSlash: true,
-};
+  images: {
+    unoptimized: true
+  }
+}
 
 export default nextConfig;
